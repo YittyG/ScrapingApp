81796d6c4a9e995dab90289211fca6e684d840b7
Chuck Gartenhaus <Chuck Gartenhaus>
2018-06-03 00:20:12 -0400

    Add project files.

---
+++ Scraper.Data/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp.Parser.Html;
+using AngleSharp.Dom;
+
+namespace Scraper.Data
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Image { get; set; }
+        public string Text { get; set; }
+
+    }
+
+    public class SData
+    {
+        public IEnumerable<Article> GetAll(string url)
+        {
+            var client = new WebClient();
+            string site = url;
+            var articleText = client.DownloadString(site);
+            var parser = new HtmlParser();
+            var jibberish = parser.Parse(articleText);
+            var articleList = jibberish.QuerySelectorAll("div.post");
+            return articleList.Select(GetArticle);
+
+
+        }
+
+        private Article GetArticle(IElement post)
+        {
+            var article = new Article();
+            var title = post.QuerySelector("a");
+            article.Title = title.TextContent;
+            article.Url = title.Attributes["href"].Value;
+
+            var pic = post.QuerySelector("img");
+            article.Image = pic.Attributes["src"].Value;
+
+            var text = post.QuerySelector("p").TextContent;
+            if(text.Length > 11)
+            {
+                article.Text = (text.Contains("Read more") ? text.Remove(text.Length - 11) : text);
+
+            }
+
+            return article;
+
+            
+
+            
+        }
+    }
+}
---
+++ Scraper/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Scraper.Data;
+
+namespace Scraper.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int? page)
+        {
+            page = page ?? 1;
+            SData data = new SData();
+            return View(data.GetAll($"http://www.thelakewoodscoop.com/news/page/{page}"));
+        }
+
+        
+    }
+}
